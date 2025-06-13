a7ffab6f8a714ec33c7fa8a683cd1f97c2446bad
handgull <gullandrea693@gmail.com>
2025-06-13 16:45:02 +0200

    flat and OnPush components by default

--- src/app/components/filters-form.ts
+++ src/app/components/filters-form.ts
@@ -1,15 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: 'app-filters-form',
+  selector: "app-filters-form",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: `
-    <p>
-      filters-form works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>filters-form works!</p> `,
+  styles: ``,
 })
-export class FiltersForm {
-
-}
+export class FiltersForm {}
--- src/app/components/temperature-chart.ts
+++ src/app/components/temperature-chart.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-temperature-chart",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: ` <p>temperature-chart works!</p>`,
   styles: ``,
