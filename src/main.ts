4cd9f0e5f8d92148a2d9e63cabc16b53317a9756
handgull <gullandrea693@gmail.com>
2025-06-13 19:24:28 +0200

    easter egg

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { App } from "./app/app";
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+console.log(
+  ` _____ ______ ______ __   __ _____   ___
+|  _  || ___ \\| ___ \\\\ \\ / /|_   _| / _ \\
+| | | || |_/ /| |_/ / \\ V /   | |  / /_\\ \\
+| | | ||    / | ___ \\  \\ /    | |  |  _  |
+\\ \\_/ /| |\\ \\ | |_/ /  | |    | |  | | | |
+ \\___/ \\_| \\_|\\____/   \\_/    \\_/  \\_| |_/
+%c                     .::.
+                  .:'  .:
+        ,MMM8&&&.:'   .:'
+       MMMMM88&&&&  .:'
+      MMMMM88&&&&&&:'
+      MMMMM88&&&&&&
+    .:MMMMM88&&&&&&
+  .:'  MMMMM88&&&&
+.:'   .:'MMM8&&&'
+:'  .:'
+'::'`,
+  "color: blue",
+);
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
